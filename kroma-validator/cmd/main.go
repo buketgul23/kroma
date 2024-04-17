229ecefb85ba62bd4257a644aecd188ccaf645bc
sm-stack <jeon0015@naver.com>
2024-04-17 18:22:48 +0900

    feat(validator): add subcommands for the new validator system

--- kroma-validator/cmd/main.go
+++ kroma-validator/cmd/main.go
@@ -59,6 +59,103 @@ func main() {
 			Usage:  "Attempt to unbond in ValidatorPool",
 			Action: balance.Unbond,
 		},
+		{
+			Name:  "approve",
+			Usage: "Approve the AssetManager to spend governance tokens",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "amount",
+					Usage:    "Amount to approve the AssetManager to spend (in wei)",
+					Required: true,
+				},
+			},
+			Action: balance.Approve,
+		},
+		{
+			Name:  "delegate",
+			Usage: "Attempt to self-delegate governance tokens",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "amount",
+					Usage:    "Amount to self-delegate (in wei)",
+					Required: true,
+				},
+			},
+			Action: balance.Delegate,
+		},
+		{
+			Name:  "initUndelegate",
+			Usage: "Initiate an undelegation of governance tokens",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "amount",
+					Usage:    "Amount to undelegate (in wei)",
+					Required: true,
+				},
+			},
+			Action: balance.InitUndelegate,
+		},
+		{
+			Name:   "finalizeUndelegate",
+			Usage:  "Finalize an undelegation of KROs",
+			Action: balance.FinalizeUndelegate,
+		},
+		{
+			Name:  "initClaimValidatorReward",
+			Usage: "Initiate a claim of validator rewards",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "amount",
+					Usage:    "The amount of rewards to claim (in wei)",
+					Required: true,
+				},
+			},
+			Action: balance.InitClaimValidatorReward,
+		},
+		{
+			Name:   "finalizeClaimValidatorReward",
+			Usage:  "Finalize a claim of validator rewards",
+			Action: balance.FinalizeClaimValidatorReward,
+		},
+		{
+			Name:  "registerValidator",
+			Usage: "Register the validator to ValidatorManager",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "amount",
+					Usage:    "The amount of assets to self-delegate (in wei)",
+					Required: true,
+				},
+				&cli.Uint64Flag{
+					Name:     "commissionRate",
+					Usage:    "The commission rate the validator sets (in percentage). Maximum 100.",
+					Required: true,
+				},
+				&cli.Uint64Flag{
+					Name:     "commissionMaxChangeRate",
+					Usage:    "The maximum changeable commission rate change (in percentage). Maximum 100.",
+					Required: true,
+				},
+			},
+			Action: balance.RegisterValidator,
+		},
+		{
+			Name:   "tryUnjail",
+			Usage:  "Attempt to unjail the validator",
+			Action: balance.TryUnjail,
+		},
+		{
+			Name:  "changeCommissionRate",
+			Usage: "Change the commission rate of the validator",
+			Flags: []cli.Flag{
+				&cli.Uint64Flag{
+					Name:     "commissionRate",
+					Usage:    "The new commission rate the validator sets (in percentage). Maximum 100.",
+					Required: true,
+				},
+			},
+			Action: balance.ChangeCommissionRate,
+		},
 	}
 
 	err := app.Run(os.Args)
